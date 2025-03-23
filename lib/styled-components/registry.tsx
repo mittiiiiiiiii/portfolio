e541df648a9fc5d80834f1c188feb9afdb81b7ea
Mitiii <srp5628comust@gmail.com>
2025-03-23 13:03:10 +0900

    styledcomponentsを導入

---
+++ lib/styled-components/registry.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import React, { useState } from 'react';
+import { useServerInsertedHTML } from 'next/navigation';
+import { ServerStyleSheet, StyleSheetManager, createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  html,
+  body {
+    max-width: 100vw;
+    overflow-x: hidden;
+    font-family: "Helvetica Neue", Arial, "Helvetica", "Yu Gothic", "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif;
+    background: rgb(218, 227, 243);
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+export default function StyledComponentsRegistry({ children }: { children: React.ReactNode }) {
+  // Only create stylesheet once with lazy initial state
+  // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
+  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+
+  useServerInsertedHTML(() => {
+    const styles = styledComponentsStyleSheet.getStyleElement();
+    styledComponentsStyleSheet.instance.clearTag();
+    return styles;
+  });
+
+  if (typeof window !== 'undefined') return children;
+
+  return (
+    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+      <GlobalStyle />
+      {children}
+    </StyleSheetManager>
+  );
+}
